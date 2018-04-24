8d01bd1c51cd67a5dffd33fb4b044bdf7355f22a
Tuo <82042108@qq.com>
2018-04-24 15:36:27 +0800

    Fix docker compose and test mysql config

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,6 @@
 FROM node:alpine
 WORKDIR /cronflow
 COPY package.json .
-RUN npm i
+RUN npm i --registry=https://registry.npm.taobao.org
 COPY . .
 CMD [ "node", "--max-old-space-size=1900", "bin/www" ]
---
+++ init/20180425.init.sql
@@ -0,0 +1,13 @@
+-- ----------------------------
+-- Table structure for `temp_data`
+-- ----------------------------
+DROP TABLE IF EXISTS `temp_data`;
+CREATE TABLE `temp_data` (
+  `id` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of temp_data
+-- ----------------------------
+INSERT INTO `temp_data` VALUES ('1');
