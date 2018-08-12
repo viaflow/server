a785a5f13506e1244eb906ef351d7e03225df023
Sirius Guo <guotuo1024@gmail.com>
2018-08-12 20:23:47 +0800

    try add flow pug page

---
+++ src/routes/flow.js
@@ -0,0 +1,21 @@
+
+
+export const AddInit = {
+    path: '/add',
+    method: 'get',
+    handler: async (req, res) => {
+        res.render();
+    },
+};
+
+export const AddSubmit = {
+    path: '/add',
+    method: 'post',
+    handler: async (req, res) => {
+
+    },
+};
+
+export const Flows = {
+
+};
