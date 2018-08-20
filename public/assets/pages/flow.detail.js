d1a7f318133b5eba7a88b47a28ed25dbabdab063
Sirius Guo <guotuo1024@gmail.com>
2018-08-20 12:52:01 +0800

    Change ignore dist to root/dist

--- public/assets/pages/flow.detail.js
+++ public/assets/pages/flow.detail.js
@@ -1,6 +1,24 @@
-$('#addNodeToRoot').click(() => {
-    top.dialog({
+/* eslint-disable */
+const pluginDialog = (flow, parent) => {
+    const option = {
         url: '/plugin/list',
-    }).show();
+        width: '76em',
+        height: '46em',
+        quickClose: true,
+        onshow: () => {
+            // 设置的iframe的scroll属性为auto
+            jQuery('.ui-dialog-content').find('iframe').attr('scrolling', 'auto');
+        },
+    };
+    if (flow !== undefined && parent !== undefined) {
+        option.url += `/${flow}/${parent}`;
+    }
+    return dialog(option);
+};
+
+jQuery('#addNodeToRoot').click(() => {
+    // add to root
+    const pluginList = pluginDialog(jQuery('#flowId').val(), 0);
+    pluginList.showModal();
     return false;
 });
