32a9f20f2a70d14a04ee6630e0aa76252bca81e5
Sirius Guo <guotuo1024@gmail.com>
2018-08-31 18:50:33 +0800

    Fix notify logic

--- public/assets/pages/plugin.list.js
+++ public/assets/pages/plugin.list.js
@@ -1,23 +1,5 @@
 /* eslint-disable */
 (function ($) {
-    // // Bind normal buttons
-    // Ladda.bind('.ladda-button', { timeout: 30 * 1000 });
-
-    // // Bind progress buttons and simulate loading progress
-    // Ladda.bind('.progress-demo .ladda-button', {
-    //     callback: function (instance) {
-    //         var progress = 0;
-    //         var interval = setInterval(function () {
-    //             progress = Math.min(progress + Math.random() * 0.1, 1);
-    //             instance.setProgress(progress);
-
-    //             if (progress === 1) {
-    //                 instance.stop();
-    //                 clearInterval(interval);
-    //             }
-    //         }, 200);
-    //     }
-    // });
 
     const l = Ladda.create(document.querySelector('.ladda-button'));
     // l.start();
@@ -42,14 +24,16 @@
                 $.Notification.notify('warning', 'top center', 'Start process...', `You could close this window and refresh later, system will install plugin asynchronously.`)
             },
             success: (data, code, xhr) => {
-                l.stop();
+                Custombox.close();
                 $.Notification.notify('success', 'top center', 'Plug-in installed', `Plug-in has already installed to system, refresh current page.`)
-                Custombox.close()
+                l.stop();
             },
             error: (xhr, textStatus, error) => {
-                l.stop();
+                Custombox.close();
+
                 $.Notification.notify('error', 'top center', 'Plug-in error', `Plug-in install has some errors are ${xhr.status}`)
                 console.log(xhr);
+                l.stop();
             }
         })
     })
